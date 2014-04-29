d69c6e9d90a383ddc422a9cd769ce35be2b551ec
leemark <leemark@gmail.com>
2014-04-28 23:18:55 -0600

    ...

--- js/expandy.js
+++ js/expandy.js
@@ -19,19 +19,28 @@ if (typeof Object.create !== 'function') {
                 self.toggleEl = options;
             } else {
                 self.toggleEl = options.toggleElement;
+                self.jqAnim = options.jqAnim;
                 self.options = $.extend({}, $.fn.makeExpander.options, options);
             }
-            self.wrapHidden(self.$el, self.toggleEl);
-            self.attachToggle(self.$el, self.toggleEl);
+            self.wrapHidden(self.$el, self.toggleEl, self.jqAnim);
+            self.attachToggle(self.$el, self.toggleEl, self.jqAnim);
         },
-        wrapHidden: function ($el, toggleEl) {
+        wrapHidden: function ($el, toggleEl, jqAnim) {
             $el.find(toggleEl).each(function(){ 
-                $(this).nextUntil(toggleEl).wrapAll('<div class="expandy expandy-hidden" />');
+                if(jqAnim){
+                    $(this).nextUntil(toggleEl).wrapAll('<div class="expandy" />');
+                }else{
+                    $(this).nextUntil(toggleEl).wrapAll('<div class="expandy expandy-hidden" />');   
+                }
             });
         },
-        attachToggle: function ($el, toggleEl) {
+        attachToggle: function ($el, toggleEl, jqAnim) {
             $el.find(toggleEl).addClass('expandy-toggle').on('click', function(){ 
-                $(this).toggleClass('expandy-hidden').next('.expandy').toggleClass('expandy-hidden');
+                if(jqAnim){
+                    $(this).toggleClass('expandy-hidden').next('.expandy').slideToggle();
+                }else{ 
+                    $(this).toggleClass('expandy-hidden').next('.expandy').toggleClass('expandy-hidden');  
+                }
             });
         }
     };
@@ -44,7 +53,8 @@ if (typeof Object.create !== 'function') {
     };
     
     $.fn.makeExpander.options = {
-        toggleElement: 'h2'
+        toggleElement: 'h2',
+        jqAnim: false
     };
     
 })( jQuery, window, document );
@@ -54,11 +64,13 @@ if (typeof Object.create !== 'function') {
 
 USAGE
 var options = {
-        toggleElement: 'h2'
+        toggleElement: 'h2',
+        jqAnim: false
     }
 $(container).makeExpander(options);
 
 // container - element containing all the toggle/expanding content
-// toggleElement - heading for each piece of hidden/displayed content. Everything between each toggleElement will be hidden.
+// toggleElement - heading for each piece of hidden/displayed content. Everything between each toggleElement will be hidden. default is <h2>
+// jqAnim - use jQuery slide animation instead of CSS to show/hide. default is false.
 
 **/
