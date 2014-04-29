dede16dc62e7062badea3753b41add726e4d60cc
leemark <leemark@gmail.com>
2014-04-28 23:21:17 -0600

    ...

--- js/expandy.js
+++ js/expandy.js
@@ -28,7 +28,7 @@ if (typeof Object.create !== 'function') {
         wrapHidden: function ($el, toggleEl, jqAnim) {
             $el.find(toggleEl).each(function(){ 
                 if(jqAnim){
-                    $(this).nextUntil(toggleEl).wrapAll('<div class="expandy" />');
+                    $(this).nextUntil(toggleEl).wrapAll('<div class="expandy" />').hide();
                 }else{
                     $(this).nextUntil(toggleEl).wrapAll('<div class="expandy expandy-hidden" />');   
                 }
